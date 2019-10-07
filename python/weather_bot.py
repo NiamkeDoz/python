83e7e0c068a7b96fed67fecaca2fbcfec02ceb11
Niamke Dozier <s1niamke@gmail.com>
2019-10-07 01:29:00 -0500

    User can pick from a list of cities

--- python/weather_bot.py
+++ python/weather_bot.py
@@ -3,9 +3,37 @@ import requests
 import time
 from bs4 import BeautifulSoup
 
+#List of cities
+cities = [
+    'Glen Carbon, IL',
+    'Edwardsville, IL',
+    'Decatur, IL',
+    'Chicago, IL',
+    'St. Louis, MO'
+]
+
+#Dictionary of city urls
+city_url = {
+    'Glen Carbon, IL': 'https://weather.com/weather/today/l/6aac383f81800632ef5ac912703b9ad03fc1c483d9d340c9ceea3b527f9d6c25',
+    'Edwardsville, IL': 'https://weather.com/weather/today/l/eeb123acf2414db84d8aeddccbb00775b33e4798ed789cd7a988ac709a698c37',
+    'Decatur, IL': 'https://weather.com/weather/today/l/4f3cdad7aac4a702031dd6d797e9225e37d054b615d9f3fbe24e9571f5aa1010',
+    'St. Louis, MO': 'https://weather.com/weather/today/l/7d11e61b33b7d790573f8183cf3280fc8158bfa60ef3f3a96d426a427f0ccd0c',
+    'Chicago, IL': 'https://weather.com/weather/today/l/e0abde3003a88dedecad92fedc96375000c16843287a51dbf2cd92f062217180' 
+}
+
+#Number of cities in list
+NumOfCities = len(cities)
+NumOfCities = int(NumOfCities)
+
+for x in range(NumOfCities):
+    print(str(x) + '. ' + cities[x])
+
+user_input = input()
+user_input = int(user_input)
+city = cities[user_input]
 
 #Request data from web 
-page = requests.get("https://weather.com/weather/today/l/eeb123acf2414db84d8aeddccbb00775b33e4798ed789cd7a988ac709a698c37")
+page = requests.get(city_url[city])
 #Parse html data
 soup = BeautifulSoup(page.text, 'html.parser')
 
