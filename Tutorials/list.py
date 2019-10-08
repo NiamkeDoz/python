6db94bb440e789b611c3cdc35031b6714ad097f5
Niamke Dozier <s1niamke@gmail.com>
2019-10-07 22:58:25 -0500

    List Tutorial

---
+++ Tutorials/list.py
@@ -0,0 +1,24 @@
+# Part 1
+# A list can be made up of zero or more elements, separated by commas, and inside of square brackets.
+empty_list = [ ]
+#print out contents of empty list
+print(empty_list)
+
+#list with contents inside
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+
+animals = ['dogs', 'cats', 'horses']
+mixture = ['Name', 1, 'Last Name']
+
+#Print values of list
+print(animals)
+print(mixture)
+
+another_empty_list = list()
+print(another_empty_list)
+
+another_empty_list.append('Name')
+print(another_empty_list)
+
+
+# Part 2
