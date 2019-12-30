0cf790bf32accfc5ab8c610fa045a8b7ec6bdba3
Niamke Dozier <s1niamke@gmail.com>
2019-12-30 00:43:33 -0600

    updates

---
+++ Tutorials/reverse.py
@@ -0,0 +1,10 @@
+#reversing a list using reverse method reverse()
+cars = [
+    'tesla', 'bmw', 'toyota', 'subaru', 'audi', 'lexus'
+]
+
+print(cars)
+cars.sort()
+print(cars)
+cars.reverse()
+print(cars)
